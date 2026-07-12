8e9069e5d3a0e9dd0171ff2ab06bef8b1d1bbd08
Diarmuid <diarmuidenright29@gmail.com>
2026-07-12 18:02:58 +0000

    Update redeploy script for systemd

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-cd ~/portfolio
-
-git pull origin main
-
+cd /root/portfolio
+git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
-
 pip install -r requirements.txt
-
-tmux kill-session -t flask_app
-
-tmux new-session -d -s flask_app "source python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
+systemctl restart myportfolio
