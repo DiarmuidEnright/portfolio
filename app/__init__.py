31df40363a02dca382a068e0cd30731549f0c50a
jessica nguyen <jessicaknguyen04@gmail.com>
2026-07-15 18:57:14 -0600

    mlh task: adding tests for database and app for partner

--- app/__init__.py
+++ app/__init__.py
@@ -43,13 +43,16 @@ def hobbies():
 def map():
     return render_template('map.html', title="Map", locations=data.LOCATIONS)
 
-mydb = MySQLDatabase(
-    os.getenv("MYSQL_DATABASE"),
-    user=os.getenv("MYSQL_USER"),
-    password=os.getenv("MYSQL_PASSWORD"),
-    host=os.getenv("MYSQL_HOST"),
-    port=3306
-)
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+    )
 
 class TimelinePost(Model):
     name = CharField()
@@ -65,9 +68,17 @@ mydb.create_tables([TimelinePost])
 
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
-    name = request.form['name']
-    email = request.form['email']
-    content = request.form['content']
+    name = request.form.get('name')
+    email = request.form.get('email')
+    content = request.form.get('content')
+
+    if not name or not name.strip():
+        return 'Invalid name', 400
+    if not content or not content.strip():
+        return 'Invalid content', 400
+    if not email or '@' not in email:
+        return 'Invalid email', 400
+
     timeline_post = TimelinePost.create(name=name, email=email, content=content)
     return model_to_dict(timeline_post)
 
